7c3be1a29521c23a2bb6e231c92d4186dbd272ce
vampirerachel <vampirerachel@gmail.com>
2023-03-12 12:29:02 -0400

    draft 1

---
+++ js/01-gallery.js
@@ -0,0 +1,34 @@
+import { simpleLightbox } from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+
+// Change code below this line
+
+
+const containerEl = document.querySelector('.gallery')
+
+galleryItems.forEach(item => {
+    const div = document.createElement('div');
+    div.classList.add('class', "gallery__item");
+    div.innerHTML = `
+<a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+    /></a> `
+    containerEl.append(div)
+    div.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(e)
+    const instance = basicLightbox.create(`
+    <img src= "${item.original}">`)
+    instance.show()
+    console.log(e.target.src);
+    instance = basicLightbox.create(`
+  <img src='${e.target.src}'/>`);
+        instance.show();
+    })
+})
