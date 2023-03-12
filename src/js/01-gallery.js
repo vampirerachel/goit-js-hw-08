527b3da4ca587eb90789dc9873a1cebf7e790e17
vampirerachel <vampirerachel@gmail.com>
2023-03-12 14:59:04 -0400

    draft 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,7 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
