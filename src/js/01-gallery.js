d7a439133c9b267249eff40e2ed5c1d71f02b466
vampirerachel <vampirerachel@gmail.com>
2023-03-12 14:29:11 -0400

    draft 2

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+const containerEl = document.querySelector('.gallery')
+
+galleryItems.forEach(item => {
+    let div = document.createElement('div');
+    div.classList.add('class', "gallery__item");
+    div.innerHTML = `
+<a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+    /></a> `
+    containerEl.append(div)
+    div.addEventListener('click', (e) => {
+    e.preventDefault()
+    const instance = basicLightbox.create(`
+    <img src= "${item.original}">`)
+    instance.show()
+    instance = basicLightbox.create(`
+    <img src='${e.target.src}'/>`);
+        instance.show();
+    })
+})
