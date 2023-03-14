2f3f98e1a4c0df76cdc6db6e252e3524e65d340b
vampirerachel <vampirerachel@gmail.com>
2023-03-13 22:57:03 -0400

    draft 5

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
