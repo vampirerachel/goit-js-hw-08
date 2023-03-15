85e054e0f17ddf7491cbda9d0c717969ec8a1e97
vampirerachel <vampirerachel@gmail.com>
2023-03-14 21:54:09 -0400

    draft 7

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,27 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-  _.throttle(e => {
-    console.log(e.target.value);
-  }, 1000)
 
-player.on("timeupdate", (event) => {
-    let currentTime = event.seconds
+player.on("timeupdate", throttle(getCurrentTime, 1000));
+
+function getCurrentTime(event){
+        let currentTime = event.seconds
     localStorage.setItem("videoplayer-current-time", currentTime)
-
-});
-
+}
+                
+                                
+                
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
     // seconds = the actual time that the player seeked to
+    console.log(typeof videoplayer-current-time)
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
             // the time was less than 0 or greater than the video’s duration
             break;
-
         default:
             // some other error occurred
             break;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
+const emailEl = document.querySelector('label [name=email]');
+const messageEl = document.querySelector('label [name=message]');
+                                                                                                                     
+const formEl = document.querySelector('form');
 
+const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+console.log(savedFormData)
+
+formEl.addEventListener('input', throttle(e => {
+    const formData = {
+        email: emailEl.value,
+        message: messageEl.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500),
+);
+
+if (savedFormData) {
+    emailEl.value = savedFormData.email;
+    messageEl.value = savedFormData.message;
+
+}
